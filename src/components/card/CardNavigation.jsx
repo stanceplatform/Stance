91aa4e485ac98a4fb3103015a767159391aa6203
Kashyap297 <kashyap29700@gmail.com>
2025-10-01 19:15:42 +0530

    fix comment drawer index issue

--- src/components/card/CardNavigation.jsx
+++ src/components/card/CardNavigation.jsx
@@ -10,10 +10,10 @@ function CardNavigation({ onNext, onPrevious }) { // Removed question prop
 
   return (
     <nav className="flex absolute inset-0 h-full overflow-hidden mx-auto max-w-[480px] z-10" id="card-navigation" >
-      <div className="flex-1 h-full" onClick={handleLeftTap} style={{ cursor: 'pointer', backgroundColor: 'rgba(0,0,0,0.1)' }}>
+      <div className="flex-1 h-full" onClick={handleLeftTap} style={{ cursor: 'pointer', }}>
         {/* Left half of the screen */}
       </div>
-      <div className="flex-1 h-full" onClick={handleRightTap} style={{ cursor: 'pointer', backgroundColor: 'rgba(0,0,0,0.1)' }}>
+      <div className="flex-1 h-full" onClick={handleRightTap} style={{ cursor: 'pointer', }}>
         {/* Right half of the screen */}
       </div>
     </nav>
--- src/components/comments/CommentDrawer.jsx
+++ src/components/comments/CommentDrawer.jsx
@@ -5,7 +5,7 @@ import OpinionThread from './OpinionThread';
 
 function CommentDrawer({ isOpen, onClose, cardId, answerOptions, onNewComment }) {
   return (
-    <div className={`fixed mx-auto inset-x-0 bottom-0 transition-transform transform ${isOpen ? 'translate-y-0' : 'translate-y-full'} bg-neutral-900 rounded-t-3xl max-w-[480px]`}>
+    <div className={`fixed z-10 mx-auto inset-x-0 bottom-0 transition-transform transform ${isOpen ? 'translate-y-0' : 'translate-y-full'} bg-neutral-900 rounded-t-3xl max-w-[480px]`}>
       <div className="flex justify-between items-center py-5">
         <div className="flex flex-col items-center w-full">
           <div id="line" className="flex w-10 rounded bg-neutral-400 min-h-[4px]" />
